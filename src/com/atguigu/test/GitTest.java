3fdca5462912f5d539561d227f091e74842ab510
Felix <1064774051@qq.com>
2020-11-27 13:16:27 +0800

    IDEA集成Git的使用！(1)

---
+++ src/com/atguigu/test/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.test;
+
+/**
+ * @version 1.0
+ * @author： Felix
+ * @date： 2020-11-27 11:36
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("这是我的第一次提交数据！");
+    }
+}
